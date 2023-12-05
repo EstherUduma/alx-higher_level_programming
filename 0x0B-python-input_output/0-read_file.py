888cf0162d20d72f643462f5ad13aebae1c67236
EstherUduma <estheruduma23@gmail.com>
2023-12-05 04:00:20 -0800

    input and output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-"""
-Module reads a text file (UTF-8) and prints it to stdout
-"""
-
 
 def read_file(filename=""):
-    """
-    Function reads a text file (UTF-8) and prints it to stdout
-    """
-    with open(filename) as f:
-        cont = f.read()
-        print(cont, end='')
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end='')
