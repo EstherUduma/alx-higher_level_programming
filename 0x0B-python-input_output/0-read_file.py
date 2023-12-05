447ab9a0f8a643ed19c4c7aeecd32d37afe99533
EstherUduma <estheruduma23@gmail.com>
2023-12-05 03:52:33 -0800

    input and output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-	with open(filename, encoding="utf-8") as file:
-		print(file.read(), end='')
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end='')
+
 
 read_file("my_file_0.txt")
