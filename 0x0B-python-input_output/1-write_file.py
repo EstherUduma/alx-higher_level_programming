2c6477fe8135943de2c211442f9699bf9dd7b865
EstherUduma <estheruduma23@gmail.com>
2023-12-05 04:19:47 -0800

    question 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """
-Writes a string to a text file (UTF-8) and returns a number of characters
+Writes a string to a text file (UTF-8)
+and returns a number of characters
 """
 
 
 def write_file(filename="", text=""):
     """
-    Writes a string to a text file (UTF-8) and returns a number of characters
+    Writes a string to a text file (UTF-8)
+    and returns a number of characters
     """
     with open(filename, mode="w", encoding="utf-8") as file:
-        file.write(text)
+        return file.write(text)
