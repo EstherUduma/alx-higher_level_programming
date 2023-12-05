1817ae883bacfa1bb0816bfdac6470fbffacbb60
EstherUduma <estheruduma23@gmail.com>
2023-12-05 04:17:28 -0800

    question 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Writes a string to a text file (UTF-8) and returns a number of characters
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF-8) and returns a number of characters
+    """
+    with open(filename, mode="w", encoding="utf-8") as file:
+        file.write(text)
