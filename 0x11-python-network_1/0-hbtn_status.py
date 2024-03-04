5d3da62b39eb970438d8705476c6a22758b37b35
Esther Uduma <estheruduma23@gmail.com>
2024-03-04 10:36:04 +0000

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body_response = response.read()
+    utf8_content = body_response.decode('utf-8')
+
+print("Body response:")
+print("\t- type:", type(body_response))
+print("\t- content:", body_response)
+print("\t- utf8 content:", utf8_content)
